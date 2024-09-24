d5ca515db5c47f15455b01d4f4e647a2a40f78be
Saklain Mostak <103751390+Saklain-Mostak@users.noreply.github.com>
2024-09-24 22:32:18 +0600

    Update news_api.dart

--- lib/http/news_api.dart
+++ lib/http/news_api.dart
@@ -1,5 +1,5 @@
 class NewsApi {
-  static const String apiKey = "e1742ffd1ddc40baba9f25b606b9301e";
+  static const String apiKey = "";
 
   static const String endpointBangladesh = "bangladesh&apiKey=$apiKey";
   static String baseurl =
@@ -9,6 +9,6 @@ class NewsApi {
 
 
 
-static const String headlinebaseurl="https://newsapi.org/v2/everything?q=world&from=2024-08-17&to=2024-08-18&sortBy=popularity&apiKey=e1742ffd1ddc40baba9f25b606b9301e";
+static const String headlinebaseurl="";
 
 }
